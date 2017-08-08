83c996a95f85c894b198100a92dd425cf059bf26
Gregorio Galante <gregogalante@gmail.com>
2017-08-08 08:35:47 +0200

    write a new test

---
+++ tests/BodyParser.test.js
@@ -0,0 +1,14 @@
+const BodyParser = require('../index').BodyParser
+
+const bodyParser = new BodyParser()
+
+test('Parse a JSON', () => {
+  const json = { hello: 'world' }
+  const string = JSON.stringify(json)
+  // parse a json
+  bodyParser.jsonParse(string, (err, data) => {
+    expect(err).toBe(null)
+    expect(typeof data).toBe('object')
+    expect(data).toEqual(json)
+  })
+})
